2c41c3300dace648b7249e5a2b7783240819dff4
Erik Tank <erik.tank@ticketmaster.com>
2017-04-11 10:50:51 -0700

    Initial Java version check in

---
+++ java/README.md
@@ -0,0 +1,21 @@
+# NAME
+
+Erik - Erik's debugging methods - Java edition
+
+# Description
+
+Real README to come.  
+
+**NOTE** This is not a port from the Perl version, but is (will be) heavily influenced by it.
+
+# AUTHOR
+
+Erik Tank, tank@jundy.com
+
+# COPYRIGHT AND LICENSE
+
+Copyright (C) 2017 by Erik Tank
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself, either Perl version 5.14.2 or,
+at your option, any later version of Perl 5 you may have available.
---
+++ java/com/jundy/Erik.java
@@ -0,0 +1,49 @@
+package com.jundy;
+
+public class Erik {
+    private static Boolean printEnabled = true;
+
+    public static void enable()  { printEnabled = false; }
+    public static void disable() { printEnabled = false; }
+
+    public static void explainByteArray(byte[] data) { explainByteArray(data, "Byte Data", 4); }
+    public static void explainByteArray(byte[] data, String label) { explainByteArray(data, label, 4); }
+    public static void explainByteArray(byte[] data, String label, Integer frameOffset) {
+        StringBuffer sb = new StringBuffer();
+        for (byte x : data) {
+            if (sb.length() > 0) {
+                sb.append(',').append(x);
+            }
+            else {
+                sb.append(x);
+            }
+        }
+        print(label + ":data: " + sb, frameOffset);
+        print(label + ":string: " + new String(data), frameOffset);
+    }
+
+
+    public static void ping() { print("", 3); }
+    public static void log(String data) { print(data, 3); }
+    public static void print(String data) { print(data, 3); }
+    private static void print(String data, Integer frameOffset) {
+        if (printEnabled) {
+            StackTraceElement frame = Thread.currentThread().getStackTrace()[frameOffset];
+            System.out.println(frame.getClassName() + " -- " + frame.getLineNumber() + ": " + data);
+        }
+    }
+
+    public static void say(String data) {
+        if (printEnabled) {
+            System.out.println(data);
+        }
+    }
+}
+
+
+/*
+Version info:
+    Why list the version in the file itself?  So it's easier to see if you are current and I'm too lazy to do it any other way.
+20170411 - First check in
+
+ */
